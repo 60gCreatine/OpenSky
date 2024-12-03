8a2c443164407581114c0f96506757d23cd317c3
60gCreatine <cphah643@gmail.com>
2024-12-03 15:34:15 +0100

    updated bbox

--- Code.R
+++ Code.R
@@ -3,18 +3,24 @@ library(jsonlite)
 
 url <- "https://opensky-network.org/api/states/all"
 bermuda_triangle_bbox <- list(
-  minLatitude = 18.4663,
-  maxLatitude = 32.3214,
-  minLongitude = -80.1903,
-  maxLongitude = -64.7574
+  lamin = 18.4663,
+  lamax = 32.3214,
+  lomin = -80.1903,
+  lomax = -64.7574
 )
 
-readRenviron(".Renviron")
-username <- Sys.getenv("username") 
-password <- Sys.getenv("password")
+#readRenviron(".Renviron")
+#username <- Sys.getenv("username") 
+#password <- Sys.getenv("password")
+
+username <- Sys.getenv("OpenSkyUsername")
+password <- Sys.getenv("OpenSkyPassword")
+
 
 response <- GET(url, query = bermuda_triangle_bbox, authenticate(username, password))
 
 data <- fromJSON(content(response, as = "text"), flatten = T)
 
 df <- as.data.frame(data$states)
+
+  
